fa85cc6ef0447fa16ec261240841d812b4c73993
drewherald <drewherald9@gmail.com>
2022-10-18 16:10:22 -0400

    created 4x4 grid with button that creates more cols
    still need to add row funtionality by retooling divs &
    js function. (in progress failsafe commit)

--- js.js
+++ js.js
@@ -3,18 +3,39 @@ const row2 = document.querySelector(".row2");
 const row3 = document.querySelector(".row3");
 const row4 = document.querySelector(".row4");
 
+const button = document.querySelector((".button"));
+let numSides = 0;
+button.addEventListener("click", (event) => {
+   numSides = prompt("How many sides?",4);
+   create(numSides);
+});
 
 
-for(let i=0;i<16;i++){
-    let div = document.createElement("div");
-    div.setAttribute("class","col");
-    if(i<4){
-        document.querySelector(".row1").appendChild(div);
-    }else if(i<8){
-        document.querySelector(".row2").appendChild(div);
-    }else if(i<12){
-        document.querySelector(".row3").appendChild(div);
-    }else{
-        document.querySelector(".row4").appendChild(div);
+
+function create(num){
+
+    for(let i=0;i<num;i++){
+        let divider = document.createElement("div");
+        div.setAttribute("id","flex");
+        div.setAttribute("class",`row${num}`);
+    }
+
+    
+    for(let i=0;i<num*4;i++){
+        let div = document.createElement("div");
+        div.setAttribute("class","col");
+        div.addEventListener("mouseover",(event)=>{
+            div.style.backgroundColor = "#00FF00";
+        });
+        if(i<num){
+            document.querySelector(".row1").appendChild(div);
+        }else if(i<num*2){
+            document.querySelector(".row2").appendChild(div);
+        }else if(i<num*3){
+            document.querySelector(".row3").appendChild(div);
+        }else{
+            document.querySelector(".row4").appendChild(div);
+        }
     }
 }
+
